d236cf5852516276cfd5129f028d8af9e0ca2fc7
apol yuzuguldu <vargakeri@icloud.com>
2023-12-11 18:03:16 +0100

    Sistemazione codice da css in line a css in foglio

--- ad.js
+++ ad.js
@@ -1,18 +1,15 @@
 function mostraMobileNascosto() {
   const mobileNascosto = document.getElementById("mobileNascosto");
-  let header=document.getElementById("header");
-  let main=document.getElementById("main");
- 
-   if (mobileNascosto.style.display === "none") {
+  let header = document.getElementById("header");
+  let main = document.getElementById("main");
+
+  if (mobileNascosto.style.display === "none") {
     mobileNascosto.style.display = "block";
     header.style.display = "none";
     main.style.display = "none";
-  
-  }else {
+  } else {
     mobileNascosto.style.display = "none";
     header.style.display = "block";
     main.style.display = "block";
-  
   }
-
 }
